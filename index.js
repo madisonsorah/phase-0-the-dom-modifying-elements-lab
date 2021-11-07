789ec0bbb9c335d5c951dbcd9e05784f533df694
madison sorah <madisonsorah@gmail.com>
2021-11-07 16:20:57 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+// Practice code
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory");
+
 const main = document.getElementById("main");
 main.remove();
 
-const newHeader = document.createElement("h1");
-newHeader.setAttribute("id", "victory");
 newHeader.textContent = "Madison is the champion";
